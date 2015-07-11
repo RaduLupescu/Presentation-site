fcb91bed1ce719ccf9aa905bf223dd5f1aa0f9dc
Protozoid <lupescu.radu@gmail.com>
2015-07-11 13:44:43 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Radsite
+
+Radu's website
+
+It's only for lols now, but it's going to get serious
+
+( ง ᵒ̌皿ᵒ̌)ง⁼³₌₃
---
+++ script.js
@@ -0,0 +1,28 @@
+"use strict"
+
+angular.module("radsite", [])
+
+.controller("radtroller", function ($scope) {
+    var date = new Date();
+
+    $scope.hours = date.getHours();
+    $scope.minutes = date.getMinutes();
+
+    $scope.greetAccordingToTime = function () {
+        if ($scope.hours > 6) {
+            if ($scope.hours < 13) {
+                return "Good morning!";
+            } else if ($scope.hours < 19) {
+                return "Good afternoon!";
+            } else if ($scope.hours < 24) {
+                return "Good evening!";
+            }
+        } else {
+            return "Good night!";
+        }
+    };
+    
+    $scope.hello = $scope.greetAccordingToTime();
+
+    console.log("hi ", $scope.hello);
+}); 
