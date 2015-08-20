2d53d3386cb0e78f6a143bdf19589e75e2a9288f
Protozoid <lupescu.radu@gmail.com>
2015-08-20 12:41:54 +0100

    Added stuff on index and nested some css

--- js/script.js
+++ js/script.js
@@ -7,12 +7,13 @@ angular.module("radsite", [])
 
     $scope.hours = date.getHours();
     $scope.minutes = date.getMinutes();
+    
     if ($scope.minutes < 10) {
         $scope.minutes = "0" + $scope.minutes;
     }
 
     $scope.greetAccordingToTime = function () {
-        if ($scope.hours > 0) {
+        if ($scope.hours >= 0) {
             if ($scope.hours < 13) {
                 return "Good morning";
             } else if ($scope.hours < 19) {
